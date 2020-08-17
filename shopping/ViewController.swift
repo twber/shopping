e4c2f96c6c99d5a2fad5965afcb342d9622f95da
twber <hctsai@umich.edu>
2020-08-17 17:04:18 +0800

    Initial Commit

---
+++ shopping/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shopping
+//
+//  Created by Bernice TSAI on 2020/8/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
